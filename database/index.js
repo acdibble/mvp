f130bf704bc00e121061ac33b27dc93460d77b37
acdibble <acdibble@gmail.com>
2018-02-16 15:48:04 -0600

    Add schema and model to database index.js

--- database/index.js
+++ database/index.js
@@ -1,7 +1,21 @@
-import mongoose from 'mongoose';
-import { MLAB_URI } from './config';
+const mongoose = require('mongoose');
+const MLAB_URI = require('./config');
+const autoIncrement = require('mongoose-auto-increment');
 
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/videos');
+const connection = mongoose.createConnection(MLAB_URI || 'mongodb://localhost/videos');
+autoIncrement.initialize(connection);
+
+const videoSchema = new Schema({
+  id: { type: Number, required: true, unique: true },
+  title: String,
+});
+videoSchema.plugin(autoIncrement.plugin, 'Video')
+
+const Video = connection.model('Video', videoSchema);
+
+const save = (params) => {
+  // TODO
+}
 
 module.exports = {
 
